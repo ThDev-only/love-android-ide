0656389647f7c5f37f19a28e910a6a2bc754bd53
Thiarley Rocha <rthiarley@gmail.com>
2025-08-01 18:37:19 -0300

    feat: new Activity on main

---
+++ app/src/main/java/org/love2d/android/MainActivity.java
@@ -0,0 +1,19 @@
+package org.love2d.android;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        LinearLayout parent = new LinearLayout(this);
+        parent.setLayoutParams(new LinearLayout.LayoutParams(-1,-1));
+        parent.setBackgroundColor(Color.BLUE);
+        
+        setContentView(parent);
+    }
+    
+}
